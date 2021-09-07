99a1115a0bf7e432dab042fddc1b78e7bc5b0de5
jmontanholi <jonunes9326@hotmail.com.br>
2021-09-07 14:08:52 -0300

    Finish details page

--- src/components/filteredPage.js
+++ src/components/filteredPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { BiLeftArrowCircle } from 'react-icons/bi';
 import { date } from '../redux/slices/covidSlice';
+import style from './filteredPage.module.css';
 
 const FilteredPage = () => {
   const state = useSelector((state) => state.covid);
@@ -10,29 +12,97 @@ const FilteredPage = () => {
     countries = state.data.dates[date].countries;
   }
   return (
-    <div>
-      <h1>
-        {state.country}
-      </h1>
-      <ul>
-        All confirmed cases:
-        <li>
-          {
-            countries[state.country].today_confirmed
-          }
-        </li>
-        <li>
-          All confirmed deaths:
-          {
-            countries[state.country].today_deaths
-          }
-        </li>
-      </ul>
-      <button type="button">
-        <NavLink to="/">
-          {'< Back'}
+    <div className={style.div}>
+      <button type="button" className={style.backBtn}>
+        <NavLink className={style.link} to="/">
+          <BiLeftArrowCircle className={style.icon} />
+          Back to Home Page
         </NavLink>
       </button>
+      <div className={style.titleDiv}>
+        <h1 className={`titleFont ${style.countryTitle}`}>
+          <span className={style.marginLeft}>
+            {state.country}
+          </span>
+        </h1>
+        <div className={style.titleInfo}>
+          <p className={`textFont ${style.titleInfoText}`}>
+            {
+              countries[state.country].today_confirmed
+            }
+          </p>
+          <h2 className={`titleFont ${style.infoTitle}`}>
+            Total Confirmed Cases
+          </h2>
+        </div>
+      </div>
+      <h2 className={`titleFont ${style.ulTitle}`}>
+        <span className={style.marginLeft}>
+          More Info
+        </span>
+      </h2>
+      <ul className={style.ul}>
+        <li className={`${style.li1} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_deaths
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Total Confirmed Deaths
+          </h2>
+        </li>
+        <li className={`${style.li2} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_recovered
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Total Confirmed Recovered
+          </h2>
+        </li>
+        <li className={`${style.li2} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_new_confirmed
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Todays Confirmed Cases
+          </h2>
+        </li>
+        <li className={`${style.li1} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_new_deaths
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Todays Deaths
+          </h2>
+        </li>
+        <li className={`${style.li1} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_new_recovered
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Todays Recovered
+          </h2>
+        </li>
+        <li className={`${style.li2} ${style.li}`}>
+          <p className={`textFont ${style.liText}`}>
+            {
+              countries[state.country].today_new_open_cases
+            }
+          </p>
+          <h2 className={`titleFont ${style.liTitle}`}>
+            Todays Opened Cases
+          </h2>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { BiRightArrowCircle } from 'react-icons/bi';
 import { date } from '../redux/slices/covidSlice';
 import { filterByCountry } from '../redux/covid/covid';
+import style from './homepage.module.css';
 
 const HomePage = () => {
-  const [select, setSelect] = useState('None');
+  const [select, setSelect] = useState('Afghanistan');
   const dispatch = useDispatch();
   const state = useSelector((state) => state.covid);
   const covidTotal = state.data.total;
@@ -19,41 +21,72 @@ const HomePage = () => {
     setSelect(event.target.value);
   };
   return (
-    <div>
+    <div className={style.div}>
       { state.loading
       && (
-      <>
-        <h2>Loading, please wait</h2>
-        <div />
-      </>
+      <div className={style.loadingDiv}>
+        <h2 className={style.loading}>Loading, please wait</h2>
+        <div className="loadingio-spinner-reload-zln0owzosys">
+          <div className="ldio-cf5nk0nv464">
+            <div>
+              <div />
+              <div />
+              <div />
+            </div>
+          </div>
+        </div>
+      </div>
       )}
       { covidTotal
       && (
       <>
-        <h1>Home Page - Covid Situation Report</h1>
-        <h2>All Confirmed Cases Worldwide</h2>
-        <p>
-          {covidTotal.today_confirmed}
-        </p>
-        <h2>All Confirmed Deaths Worldwide</h2>
-        <p>
-          {covidTotal.today_deaths}
-        </p>
-        <h2>Filter By Country:</h2>
-        <form>
-          <label htmlFor="countries">
-            Countries:
-            <select name="countries" id="countries" onChange={(e) => { handleCategory(e); }}>
-              <option value="None">None</option>
-              {
-                countriesKeys.map((country) => (
-                  <option key={country[0]} value={country[0]}>{country[0]}</option>
-                ))
-              }
-            </select>
-          </label>
+        <h1 className={`titleFont ${style.homeTitle}`}>Home Page - Covid Situation Report</h1>
+        <h2 className={`titleFont ${style.worldTitle}`}><span className={style.paddingLeft}>Worldwide Info - Up to date</span></h2>
+        <div className={`${style.generalInfo1} ${style.generalInfo}`}>
+          <p className={`textFont ${style.generalText}`}>
+            {covidTotal.today_confirmed}
+          </p>
+          <h2 className={`titleFont ${style.generalTitle}`}>Confirmed Cases</h2>
+        </div>
+        <div className={style.generalDiv}>
+          <div className={`${style.generalInfo2} ${style.generalInfo}`}>
+            <p className={`textFont ${style.generalText}`}>
+              {covidTotal.today_deaths}
+            </p>
+            <h2 className={`titleFont ${style.generalTitle}`}>Confirmed Deaths</h2>
+          </div>
+          <div className={`${style.generalInfo3} ${style.generalInfo}`}>
+            <p className={`textFont ${style.generalText}`}>
+              {covidTotal.today_recovered}
+            </p>
+            <h2 className={`titleFont ${style.generalTitle}`}>Recovered</h2>
+          </div>
+        </div>
+        <form className={style.form}>
+          <h2 className={`titleFont ${style.worldTitle}`}><span className={style.paddingLeft}>Filter By Country:</span></h2>
+          <div className={style.labelDiv}>
+            <label htmlFor="countries" className={`textFont ${style.label}`}>
+              <select className={style.select} name="countries" id="countries" onChange={(e) => { handleCategory(e); }}>
+                {
+                  countriesKeys.map((country) => (
+                    <option
+                      className={`textFont ${style.option}`}
+                      key={country[0]}
+                      value={country[0]}
+                    >
+                      {country[0]}
+                    </option>
+                  ))
+                }
+              </select>
+            </label>
+          </div>
+          <button type="button" className={style.submitBtn} onClick={() => { dispatch(filterByCountry(select)); }}>
+            <NavLink className={style.link} to={`/${select}`}>
+              <BiRightArrowCircle />
+            </NavLink>
+          </button>
         </form>
-        <button type="button" onClick={() => { dispatch(filterByCountry(select)); }}><NavLink to={`/${select}`}>Filter</NavLink></button>
       </>
       )}
     </div>
